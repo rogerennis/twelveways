2cc6586e689ad891f57091bd874c4629e4d658df
Roger Ennis <roadene@gmail.com>
2017-12-17 00:04:43 -0500

    Added custom JS to make the paypal button be full width

---
+++ assets/custom.js
@@ -0,0 +1,14 @@
+// This adds full width to the paypal button in the cart drawer
+/*
+$( document ).ready(function() {
+ 
+    $('.additional-checkout-button--paypal-express').addClass('btn--full');
+ 
+});
+*/
+
+$(function() {
+
+  $('.additional-checkout-button--paypal-express').addClass('btn--full');
+
+)};
